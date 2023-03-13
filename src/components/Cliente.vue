1b8175a9a6570a8bcbd7af17e9dae17593ce5724
Nicolas <nicolasgabriel.developer@gmail.com>
2023-03-13 20:00:57 -0300

    FEAT: Cliente.Vue
    Adicionando o primeiro componente do projeto, mas antes, limpei tudo que era desnecessário e vem por padrão em uma aplicação vue.js

---
+++ src/components/Cliente.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>Cliente aqui! - Bem vindo ao curso do programador vue!</h1>
+</template>
+
+<script>
+
+export default {
+    name:'Cliente-',
+}
+</script>
+
+<style>
+
+</style>
